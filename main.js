f188097a47f384ea27f6b3bfe8fa66ec0601cd63
hexpoland <wolaqu@poczta.onet.pl>
2017-04-23 16:53:32 +0200

    konfug workera

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-(function () {
-	new Worker('workers.js')
-})
+var myworker=new Worker('workers.js');
+myworker.onmessage=function (oEvent) {
+	console.log("Worker said:"+oEvent.data)
+}
+
+myworker.postMessage('ali');
